95dd81d3ff41ab2683b92491988cca38feffbc57
Fikayomi31 <fikayoogundijo@gmail.com>
2022-12-02 14:33:04 -0800

    correction

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 import sys
-import os
-fd = sys.stderr.fileno()
-data = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
-os.write(fd, data.encode("utf-8"))
+
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
 sys.exit(1)
