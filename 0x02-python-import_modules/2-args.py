1db464dc8d1d9a52382cc42bc2f2eda63fa44a7c
Fikayomi31 <fikayoogundijo@gmail.com>
2022-12-11 02:34:13 -0800

    it never late

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 
-import sys
-
 if __name__ == '__main__':
-    argv = sys.argv[1:]
-    argc = len(argv)
-    word = "argument"
-    if argc != 1:
-        word += 's'
-    print("{} {}{}".format(argc, word, '.' if not argc else ':'))
-    for i, arg in enumerate(argv, 1):
-        print("{}: {}".format(i, arg))
+    from sys import argv
+    if len(argv) == 1:
+        print("0 arguments")
+    elif len(argv) == 2:
+        print("{:d} argument:".format(len(argv) - 1))
+    else:
+        print("{:d} arguments:".format(len(argv) - 1))
+    for i, arg in enumerate(argv[1:], start=1):
+        print("{:d} {:s}".format(i, arg))
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-if __name__ = "__main__":
+if __name__ == "__main__":
     import hidden_4
 
     for attr in dir(hidden_4):
