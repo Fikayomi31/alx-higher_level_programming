ae799171ba76b5e77da3af3e9f58795b899943f7
Fikayomi31 <fikayoogundijo@gmail.com>
2022-12-26 02:25:31 -0800

    remove char

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    new_string = my_string.translate({ord(j):None for j in 'cC'})
+    new_string = my_string.translate({ord(j): None for j in 'cC'})
     return new_string
