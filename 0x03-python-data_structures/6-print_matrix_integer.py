5f7870584696ead1eb870831badebb2b3ba478b0
Fikayomi31 <fikayoogundijo@gmail.com>
2022-12-26 02:39:11 -0800

    matrix correction

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for col in row:
-            print("{:d}".format(col), end='' if col != row[-1] else"")
+            print("{:d} ".format(col), end='' if col != row[-1] else"")
         print()
