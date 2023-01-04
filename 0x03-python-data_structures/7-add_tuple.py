e30a6abfca0a2dcf66048a745db43fd223076e74
Fikayomi31 <fikayoogundijo@gmail.com>
2023-01-04 00:46:24 -0800

    return

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,5 +5,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
     tuple_1 = tuple_a + (0, 0)
     tuple_2 = tuple_b + (0, 0)
     result = tuple_1[0] + tuple_2[0], tuple_1[1] + tuple_2[1]
-    
     return result
