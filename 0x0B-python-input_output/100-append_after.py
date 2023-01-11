f490027ea44ebefb07d316461f1204f825273a1d
Fikayomi31 <fikayoogundijo@gmail.com>
2023-01-11 15:25:23 -0800

    file insertion

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module define a text file insertion function."""
+
+
+def append_after(filename="", search_string="", new_string):
+    """Insects text after each line containing a given string in a file
+    """
+    text = ""
+    with open(filename) as f:
+        for line in f:
+            text += line
+            if search_string in line:
+                text += new_string
+    with open(filename, "w") as w:
+        w.write(text)
