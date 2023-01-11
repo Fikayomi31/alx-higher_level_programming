eb80ea40eda382b334a07849aaa5b96c456f85c4
Fikayomi31 <fikayoogundijo@gmail.com>
2023-01-10 16:36:52 -0800

    tired

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module adds all arguments to a python list and save them to a file."""
+"""Add all arguments to a Python list and save them to a file."""
 import sys
 
 if __name__ == "__main__":
