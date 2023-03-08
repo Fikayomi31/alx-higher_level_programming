6583a895785f9594cb1945a7649980590f6c40f1
Fikayomi31 <fikayoogundijo@gmail.com>
2023-03-08 04:12:16 -0800

    reverse

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 for i in range(ord('z'), ord('a') - 1, -1):
     if i % 2 == 0:
-            print(chr(i), end='')
+        diff = 0
     else:
-        print(chr(i - 32), end='')
+        diff = 32
+    print("{}".format(chr(i - diff)), end='')
