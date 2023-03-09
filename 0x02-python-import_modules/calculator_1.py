ee6a68b1d6ac13ceb45ea0aac3fdbc0e8cd29d1d
Fikayomi31 <fikayoogundijo@gmail.com>
2023-03-09 06:04:58 -0800

    calculator

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+    
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
+
+
+def sub(a, b):
+    """My substraction function
+
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a - b
+    """
+    return (a - b)
+
+def mul(a, b):
+    """My multiplication function
+
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a * b
+    """
+    return (a * b)
+
+
+def div(a, b):
+    """My division function
+
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a / b
+    """
+
+    return (a / b)
