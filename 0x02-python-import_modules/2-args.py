5482ed67bf05151f092fe5599e3e546b569d2bdf
Fikayomi31 <fikayoogundijo@gmail.com>
2023-03-09 06:27:20 -0800

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import sys
-    
-    count = len(sys.argv) - 1
 
+    count = len(sys.argv) - 1
     if count == 0:
         print("0 arguments:")
     elif count == 1:
