c812a51ed95152cccddc4eba3d677ce77d62316c
Fikayomi31 <fikayoogundijo@gmail.com>
2023-03-09 12:22:34 -0800

    infinite

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import sys
-result = 0
-for arg in sys.argv[1:]:
-    result += int(arg)
-print(result)
+if __name__ = '__main__':
+    import sys
+    result = 0
+    for arg in sys.argv[1:]:
+        result += int(arg)
+    print("{}".format(result))
