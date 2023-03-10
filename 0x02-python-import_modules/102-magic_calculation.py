211cce50cba221870450ecc6da143ef068ef422d
Fikayomi31 <fikayoogundijo@gmail.com>
2023-03-10 11:09:35 -0800

    magic

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import dis
+
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
 
@@ -11,4 +11,3 @@ def magic_calculation(a, b):
 
     else:
         return sub(a, b)
-dis.dis(magic_calculation)
