ead5351c89d5893b52ae44529abd1c241adc3313
Fikayomi31 <fikayoogundijo@gmail.com>
2023-03-10 11:07:24 -0800

    magic

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
+import dis
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
 
-    def magic_calculation(a, b):
-        from magic_calculation_102 import add, sub
-        if a < b:
-            c = add(a, b)
-            for i in range(4, 6):
-                c = add(c, i)
-            return (c)
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return (c)
 
-        else:
-            return sub(a, b)
+    else:
+        return sub(a, b)
+dis.dis(magic_calculation)
