9ef139b9c45e7ea26f81e744be7024c18c21a79c
Fikayomi31 <fikayoogundijo@gmail.com>
2023-03-10 11:00:15 -0800

    magic

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -7,6 +7,7 @@ if __name__ == '__main__':
             c = add(a, b)
             for i in range(4, 6):
                 c = add(c, i)
-                return (c)
+            return (c)
+
         else:
             return sub(a, b)
