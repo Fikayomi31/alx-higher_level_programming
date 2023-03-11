9d101000aee297e9b9516f9c54173bdb6ab76911
Fikayomi31 <fikayoogundijo@gmail.com>
2023-03-11 01:53:05 -0800

    element

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
+def replace_in_list(my_list, idx, new_element):
     if idx < 0:
         return my_list
     if idx > len(my_list):
         return my_list
     else:
-        my_list[idx] = element
+        my_list[idx] = new_element
         return my_list
