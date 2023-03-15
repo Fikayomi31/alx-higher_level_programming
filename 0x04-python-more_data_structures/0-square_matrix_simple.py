40eca63912639a3f63abdb6fa3ae58fc92aa210d
Fikayomi31 <fikayoogundijo@gmail.com>
2023-03-15 15:49:38 -0700

    matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    new_matrix = [list(map(lambda x: x ** 2, row)) for row in matrix]
+    return new_matrix
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,1 @@
+# 0x04. Python - More Data Structues: Set, Ductionary
