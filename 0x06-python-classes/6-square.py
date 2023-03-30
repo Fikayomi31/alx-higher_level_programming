245a6e6455ee65f93878bafdd8388f3a16d6be6c
Fikayomi31 <fikayoogundijo@gmail.com>
2023-03-30 15:42:25 -0700

    square_class_6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -55,7 +55,7 @@ class Square:
             print()
             return
         [print("") for i in range(0, self.__position[1])]
-        for i in range(0, self.__size:
+        for i in range(0, self.__size):
                 [print(" ", end="") for j in range(0, self.__position[0])]
                 [print("#", end="") for k in range(0, self.__size)]
                 print()
