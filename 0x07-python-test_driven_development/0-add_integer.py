f792aea920e889cf9d35797f8c99c1adcffde4bf
Fikayomi31 <fikayoogundijo@gmail.com>
2023-04-02 06:13:59 -0700

    add

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+    """Addition function
+    Args:
+        a: can be integer or float
+        b: can be integer or float
+    Raise:
+        TypeError: if a and b is not integer or float
+    Return: the addition of a and b
+    """
+    if not isinstance(a, (int, float):
+            raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,2 @@
+# 0x07. Python - Test-driven development
+0. Write a function that adds 2 integers.
