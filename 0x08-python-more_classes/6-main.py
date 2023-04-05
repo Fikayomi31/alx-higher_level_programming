32d45b4f8b8b50998d1d42d5c06a01edc7a658ff
Fikayomi31 <fikayoogundijo@gmail.com>
2023-04-05 15:25:02 -0700

    6-rect

--- 0x08-python-more_classes/6-main.py
+++ 0x08-python-more_classes/6-main.py
@@ -3,10 +3,10 @@ Rectangle = __import__('6-rectangle').Rectangle
 
 my_rectangle_1 = Rectangle(2, 4)
 my_rectangle_2 = Rectangle(2, 4)
-print("{:d} instance of Rectangle".format(Rectangle.number_of_instance))
+print("{:d} instance of Rectangle".format(Rectangle.number_of_instances))
 
 del my_rectangle_1
-print("{:d} instance of Rectangle".format(Rectangle.number_of_instance))
+print("{:d} instance of Rectangle".format(Rectangle.number_of_instances))
 
 del my_rectangle_2
-print("{:d} instance of Rectangle".format(Rectangle.number_of_instance))
+print("{:d} instance of Rectangle".format(Rectangle.number_of_instances))
