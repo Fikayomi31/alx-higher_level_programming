16760030a974cf41a3d6f1cec2068f3681f33964
Fikayomi31 <fikayoogundijo@gmail.com>
2023-04-12 15:36:38 -0700

    add

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
 
-    item = load_from_json_file("add_item.json")
+    item = load_from_json_file(add_item.json)
     item.extend(sys.argv[1:])
-    save_to_json_file(item, "add_item.json")
+    save_to_json_file(item, add_item.json)
