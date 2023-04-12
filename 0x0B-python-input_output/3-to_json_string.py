846073299c2546ba52254836cf97c006b9170e72
Fikayomi31 <fikayoogundijo@gmail.com>
2023-04-12 11:25:03 -0700

    json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module return json representation of a string"""
+import json
+
+def to_json_string(my_obj):
+    """print json of my_obj"""
+    return json.dumps(my_obj)
