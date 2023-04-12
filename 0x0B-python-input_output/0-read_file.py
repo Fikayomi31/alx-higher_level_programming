e4b24c62d02847d511eb2470309c07c391bc97e4
Fikayomi31 <fikayoogundijo@gmail.com>
2023-04-12 09:59:03 -0700

    read

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/pyn3
+"""This module define a text read function"""
 
-"""read module"""
+
 def read_line(filename=""):
-    with open("filename", mode='r', encoding="utf-8") as f:
+    """printing the content of the file"""
+    with open(filename, encoding="utf-8") as f:
         print(f.read())
