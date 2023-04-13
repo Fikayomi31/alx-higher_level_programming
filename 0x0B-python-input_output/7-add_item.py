bf423351308a90b49ad69c4ee78dc6192a5c1fe4
Fikayomi31 <fikayoogundijo@gmail.com>
2023-04-13 00:55:43 -0700

    add

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-
+"""This module import function to add to agrument"""
 import sys
-import json
 
-def save_to_json_file(my_obj, filename):
-    with open(filename, 'w') as f:
-        json.dump(my_obj, f)
+if __name__ = "__main__":
+    save_to_json_file = \
+        __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-def load_from_json_file(filename):
-    with open(filename, 'r') as f:
-        return json.load(f)
 
-# Load existing list from file, if available
-try:
-    my_list = load_from_json_file('add_item.json')
-except FileNotFoundError:
-    my_list = []
-
-# Add command line arguments to the list
-for arg in sys.argv[1:]:
-    my_list.append(arg)
-save_to_json_file(my_list, 'add_item.json')
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
