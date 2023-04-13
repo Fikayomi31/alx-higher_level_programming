009ed807a917a01dddc1fa182fa047b705c6428a
Fikayomi31 <fikayoogundijo@gmail.com>
2023-04-13 00:57:53 -0700

    add

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,7 +8,6 @@ if __name__ = "__main__":
     load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
 
-
     try:
         items = load_from_json_file("add_item.json")
     except FileNotFoundError:
