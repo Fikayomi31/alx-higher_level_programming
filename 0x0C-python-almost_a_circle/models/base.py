43872554ffa43bdd8a8ed5e3a15ffa8e0d0a9dd0
Fikayomi31 <fikayoogundijo@gmail.com>
2023-04-16 12:55:25 -0700

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -8,5 +8,8 @@ class Base:
 
     def __init__(self, id=None):
         """Initializing the class base"""
-        if id != None:
-
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
