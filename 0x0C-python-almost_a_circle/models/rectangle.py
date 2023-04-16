c29aec2a178c5d34e186c9aff3cf378ded035a49
Fikayomi31 <fikayoogundijo@gmail.com>
2023-04-16 16:32:48 -0700

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """Defining a class rectangle"""
 
+from models.base import Base
 
-class Rectangle:
+
+class Rectangle(Base):
     """Representing a class rectangle"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
