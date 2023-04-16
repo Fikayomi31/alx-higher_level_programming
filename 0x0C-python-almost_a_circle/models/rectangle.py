ec695a78bd32af6d8b85b8c01325fb0e61754fa9
Fikayomi31 <fikayoogundijo@gmail.com>
2023-04-16 16:53:34 -0700

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -29,7 +29,17 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        """setting the width of the rectangle"""
+        """setting the width of the rectangle
+        Args:
+            value: value of the width
+        Raise:
+            TypeError: if the value is not an integer
+            ValueError: if value is < 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if self.__width <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -39,7 +49,17 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        """setting the height of the rectangle"""
+        """setting the height of the rectangle
+        Args:
+            value: value of the height
+        Raise:
+            TypeError: if the height is not an integer
+            ValueError: if the height is < 0
+        """
+        if not isinstance(value int):
+            raise TypeError("height must be integer")
+        if self.__height <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -49,7 +69,17 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        """setting the value for x"""
+        """setting the value for x
+        Args:
+            value: value of x
+        Raise:
+            TyoeError: if x is not an integer
+            ValueError: if x < 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x muat be >= 0")
         self.__x = value
 
     @property
@@ -59,5 +89,19 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
-        """setting the value for y"""
+        """setting the value for y
+        Args:
+            value: value for y
+        Raise:
+            TypeError: if y is not an intege
+            ValueError: if y < 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """return area of rectangle class"""
+        return self.__width * self.__height
