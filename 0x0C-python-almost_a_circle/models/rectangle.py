cc3732736ba2de37e7ae0a815bd225579e3f49ba
Fikayomi31 <fikayoogundijo@gmail.com>
2023-04-17 13:48:35 -0700

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -58,7 +58,7 @@ class Rectangle(Base):
             ValueError: if the height is < 0
         """
         if not isinstance(value, int):
-            raise TypeError("height must be integer")
+            raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
@@ -98,7 +98,7 @@ class Rectangle(Base):
             ValueError: if y < 0
         """
         if not isinstance(value, int):
-            raise TypeError("x must be an integer")
+            raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
