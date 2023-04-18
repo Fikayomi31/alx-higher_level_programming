c1cde3895490de3e44f52350ed504496d4b51bdd
Fikayomi31 <fikayoogundijo@gmail.com>
2023-04-18 04:23:12 -0700

    rect

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -124,3 +124,32 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x,
                                                        self.y, self.width,
                                                        self.height)
+
+    def update(self, *args):
+        """agrument to each attribute
+        Args:
+            args: arugument
+                - 1st argument id attribute
+                - 2nd argument width attribute
+                - 3rd argument height attribute
+                - 4th argument x attribute
+                - 5th argument y attribute
+        """
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    """if args is None"""
+                    if arg is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.width = arg
+                elif a == 2:
+                    self.height = arg
+                elif a == 3:
+                    self.x = arg
+                elif a == 4:
+                    self.y = arg
+            a += 1
