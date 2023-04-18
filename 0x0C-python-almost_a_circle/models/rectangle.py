d569072ae1705dcc42e5f385b5994eddb7a4e07d
Fikayomi31 <fikayoogundijo@gmail.com>
2023-04-17 23:18:01 -0700

    rect

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -81,7 +81,7 @@ class Rectangle(Base):
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
         if value < 0:
-            raise ValueError("x muat be >= 0")
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
