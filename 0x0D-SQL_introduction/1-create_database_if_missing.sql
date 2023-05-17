c1980581c805522b393ff20619a95a94eedc17dd
Fikayomi <fikayoogundijo@gmail.com>
2023-05-17 15:58:27 -0700

    new_server

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
--- Create the database hbtn_0c_0 in MYSQL server
-CREATE DATABASE IF NOT EXITS `hbtn_0c_0`;
+-- Create the database hbtn_0c_0 in MYSQL server.
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
