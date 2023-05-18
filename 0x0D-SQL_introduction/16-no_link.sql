089615380111475e35c0a8d2bd794df0fa8ca561
Fikayomi <fikayoogundijo@gmail.com>
2023-05-18 16:04:32 -0700

    list_name

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,6 @@
+--  lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
+-- In descending order
+SELECT score, name
+FROM second_table
+WHERE name != ""
+ORDER BY score DESC
