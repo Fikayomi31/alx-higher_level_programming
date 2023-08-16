50d75d0fd9332e12dd33d70127c0e78759c0a021
Fikayomi31 <fikayoogundijo@gmail.com>
2023-08-16 22:56:57 +0000

    state table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- Create database hbtn_0d_usa and table state
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+	id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL);
