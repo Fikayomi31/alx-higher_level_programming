7dc45db152cfe40449ebcf441689173d7d26a01c
Fikayomi31 <fikayoogundijo@gmail.com>
2023-08-16 23:11:22 +0000

    cities ta le

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- create database hbtn_0d_usa and table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+	id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	FOREIGN KEY(state_id) REFERENCES states(id));
