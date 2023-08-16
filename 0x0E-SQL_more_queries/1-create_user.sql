9f22a1b18485a0448831fdc3a90fe9bca2b3962a
Fikayomi31 <fikayoogundijo@gmail.com>
2023-08-16 22:12:17 +0000

    create user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Creates the Mysql server user_0d_
+-- grants all privileges and password
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANTS ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
