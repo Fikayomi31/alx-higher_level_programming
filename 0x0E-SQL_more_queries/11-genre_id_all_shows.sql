f1b64cd7a887067cbaa677b94fda27b91f241050
Fikayomi31 <fikayoogundijo@gmail.com>
2023-08-17 17:05:54 +0000

    genre id

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- Lists all shows contained in hbtn_0d_tvshows to genre_id
+-- And title
+SELECT title, genre_id
+FROM tv_shows LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY title, genre_id;
