8c3e757aa27bf53e20d8f9cf864a18c5c5de5b40
Fikayomi31 <fikayoogundijo@gmail.com>
2023-08-17 17:20:34 +0000

    no genre

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- Lists all sbows contained in hbtn_0d_tvshows without a genre linked
+SELECT title, genre_id
+FROM tv_shows LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id is NULL
+ORDER BY title, genre_id;
