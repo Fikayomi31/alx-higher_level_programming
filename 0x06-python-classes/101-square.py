dfdfdb626dad3f36f0ca8ec3f0b1c0e56501f447
Fikayomi31 <fikayoogundijo@gmail.com>
2023-08-31 09:58:25 +0000

    print Square instance"

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""Defining the class square"""
+
+
+class Square:
+    """Representation of the class"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """ Initializing the class"""
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Return the size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setting the value for the size
+        Args:
+            value: value for the size
+        Raise:
+            TypeError: if size not an integer
+            ValueError: if size is < 
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__posiition
+
+    @position.setter
+    def position(self, value):
+        """Setting the value for the position
+        Args:
+            value: value for the position
+        Raise:
+            TypeError: if position is not tuple or interger
+        """
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integer")
+        if not all(isinstance(num, int) and num >= 0 for num in value):
+            raise TypeError("position must be a tuple of 2 positive integer")
+        self.__position = value
+
+    def area(self):
+        """Return current square root"""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """print the square with character #"""
+        my_square = ""
+        if self.__size == 0:
+            my_square = "\n"
+
+        else:
+            for i in range(self.__position[1]):
+                my_square = "\n"
+            for i in range(self.__size):
+                my_square += " " * self.__position[0] + "#" * self.__size + "\n"
+            return my_square
+
+    def __str__(self):
+        """str representation of Square class"""
+        return self.my_print()
