5e018f828f2ce7552978ae7f7b1813c34897a48a
Fikayomi31 <fikayoogundijo@gmail.com>
2023-09-11 18:45:31 +0000

    updated task 3wq

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,11 +1,3 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2);
-
-if (args[0]) {
-  args.forEach((arg, index) => {
-    console.log(arg);
-  });
-} else {
-  console.log('No argument');
-}
+console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
