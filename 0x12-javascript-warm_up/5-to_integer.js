b40bcad10129631b7784f542e250a0db395284a2
Fikayomi31 <fikayoogundijo@gmail.com>
2023-09-11 21:12:35 +0000

    for loop

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
-const args = Math.floor(Number(process.argv[2]))
+const args = Math.floor(Number(process.argv[2]));
 console.log(isNaN(args) ? 'Not a number' : `My number: ${args}`);
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+const string = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < string.length; i++) {
+  console.log(string[i]);
+}
