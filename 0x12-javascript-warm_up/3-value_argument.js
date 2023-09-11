262c43a654c490425474f343f765291378d42f5b
Fikayomi31 <fikayoogundijo@gmail.com>
2023-09-11 17:54:23 +0000

    value of argument

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+if (args[0]) {
+  args.forEach((arg, index) => {
+    console.log(`${arg}`);
+  });
+} else {
+  console.log('No argument');
+}
