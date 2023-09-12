292ba438a852fdbd02868a5eeb2b51139071e36d
Fikayomi31 <fikayoogundijo@gmail.com>
2023-09-12 21:43:51 +0000

    rectangle task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,4 +1,19 @@
 #!/usr/bin/node
 
 class Rectangle {
-	constructor(
+	constructor(w, h) {
+		if (w > 0 && h > 0) {
+			this.width = w;
+			this.height = h;
+		}
+	}
+	print() {
+		for (let i = 0; i < this.height; i++) {
+			let row = '';
+			for (let i = 0; i < this.width; i++)
+				row += 'X';
+			console.log(row);
+		}
+	}
+}
+module.exports = Rectangle
