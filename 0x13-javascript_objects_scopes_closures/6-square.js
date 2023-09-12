a2d2554325025751c73e665a0e1b250a19f6db7b
Fikayomi31 <fikayoogundijo@gmail.com>
2023-09-12 23:29:18 +0000

    square #6 correction

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,19 +3,20 @@
 const SquareBase = require('./5-square');
 
 class Square extends SquareBase {
-	constructor (size) {
-		super(size, size);
-	}
-	charPrint(c) {
-		const value = c === undefined ? 'X': c;
-		for (let i = 0; i < this.height; i++) {
-			let row = '';
-			for (let i = 0; i < this.width; i++) {
-				row += value
-			}
-			console.log(row);
-		}
-	}
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    const value = c === undefined ? 'X' : c;
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let i = 0; i < this.width; i++) {
+        row += value;
+      }
+      console.log(row);
+    }
+  }
 }
 
 module.exports = Square;
