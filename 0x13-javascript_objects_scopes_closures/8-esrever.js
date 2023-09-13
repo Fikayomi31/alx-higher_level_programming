e71969c8ad3f1962588bb8dae07db491ceefcc91
Fikayomi31 <fikayoogundijo@gmail.com>
2023-09-13 00:17:35 +0000

    reversed #8 correction

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  const reverseArray = [];
+  const reverseList = [];
   for (let i = list.length - 1; i >= 0; i--) {
-    reverseArray.push(list[i]);
+    reverseList.push(list[i]);
   }
-  console.log(reverseArray);
+  return (reverseList);
 };
