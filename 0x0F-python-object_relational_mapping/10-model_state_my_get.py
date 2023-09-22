c5630c9911f9e7bf581e9d577bb2f313f42cea2f
Fikayomi31 <fikayoogundijo@gmail.com>
2023-09-22 16:26:41 -0700

    model state

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -11,7 +11,7 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
                             sys.argv[1], sys.argv[2], sys.argv[3]),
-                            pool_pre_ping=True)
+                           pool_pre_ping=True)
     s_name = sys.argv[4]
     Session = sessionmaker(bind=engine)
     session = Session()
