f2ba23a4cad5ef462de3bf6bb056d01731c8d774
Fikayomi31 <fikayoogundijo@gmail.com>
2023-09-29 22:03:55 +0000

    reslonse header value

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
     url = sys.argv[1]
 
     response = requests.get(url)
-    result = response.headers['X-Request-Id']
+    result = response.headers.get('X-Request-Id')
     print(result)
