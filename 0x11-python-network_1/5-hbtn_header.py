97fae3bd52fba40368e1c075f052d389b8b33fa9
Fikayomi31 <fikayoogundijo@gmail.com>
2023-09-29 21:54:40 +0000

    reslonse header value

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -6,8 +6,8 @@ X-Request-Id in the response header
 import requests
 import sys
 
-if __name__  == "__main__":
-    
+if __name__ == "__main__":
+
     url = sys.argv[1]
 
     response = requests.get(url)
