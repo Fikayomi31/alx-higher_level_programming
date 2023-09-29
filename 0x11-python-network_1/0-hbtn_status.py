1814494ecbc2ec816a79affc2106e619a4596a13
Fikayomi31 <fikayoogundijo@gmail.com>
2023-09-29 15:25:26 +0000

    removing whitespace from the Get rquest

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,17 +5,17 @@ import urllib.request
 if __name__ == "__main__":
 
     url = 'https://alx-intranet.hbtn.io/status'
-    
+
     """HTTP GET request to the url"""
     response = urllib.request.Request(url)
     with urllib.request.urlopen(response) as respons:
-        
+
         """read the content as bytes"""
         content = respons.read()
-        
+
         """Decode the content to utf8"""
         content_utf8 = content.decode('utf-8')
-        
+
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
