6f0c6f6db447e95e7c5a6ec3a947958fd9367d4a
Fikayomi31 <fikayoogundijo@gmail.com>
2023-09-29 21:52:18 +0000

    reslonse header value

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Takes in url, sends a request to the url and display the value of
+X-Request-Id in the response header
+"""
+
+import requests
+import sys
+
+if __name__  == "__main__":
+    
+    url = sys.argv[1]
+
+    response = requests.get(url)
+    result = response.headers['X-Request-Id']
+    print(result)
