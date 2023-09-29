19fc733b521d5e678e6a65892b2a60fd753cd296
Fikayomi31 <fikayoogundijo@gmail.com>
2023-09-29 00:38:23 +0000

    curl only method

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that take in a url and display all HTTP method the server will accept
-curl -sIX "$1" | grep -i 'allow' | cut -d ' ' -f 2-
+curl -sIX OPTIONS "$1" | grep -i 'allow' | cut -d ' ' -f 2-
