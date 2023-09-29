3823433114718f344755f52b2a266487af299628
Fikayomi31 <fikayoogundijo@gmail.com>
2023-09-29 22:13:59 +0000

    lost an email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Takes a url and an email, send a post request to passed url with
+the email as aparameter and finallh display bodgbof response
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+
+    url = sys.argv[1]
+    data = {'email': sys.argv[2]}
+
+    r = requests.post(url, data)
+    response = r.read().decode('utf-8')
+    print(response)
