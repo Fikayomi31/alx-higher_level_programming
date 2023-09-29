e8daa2d486ccbfb817aed83ea266b9027864e093
Fikayomi31 <fikayoogundijo@gmail.com>
2023-09-29 18:53:13 +0000

    response header value

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Scriptbthat take in a url, send a request to the url and
+display value of the X-Requect-id variable found in the header
+"""
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+
+    url = sys.argv[1]
+
+    """HTTP to GET request to the url and urlopen to open it"""
+    response = urllib.request.Request(url)
+    with urllib.request.urlopen(response) as respons:
+        print(respons.getheader("X-Request_Id"))
