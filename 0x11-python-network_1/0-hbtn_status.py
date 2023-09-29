f95f24c4a7ee9df6387a08e558b5f2dfa9696928
Fikayomi31 <fikayoogundijo@gmail.com>
2023-09-29 15:21:27 +0000

    http GET request with readme

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import urllib.request
+
+if __name__ == "__main__":
+
+    url = 'https://alx-intranet.hbtn.io/status'
+    
+    """HTTP GET request to the url"""
+    response = urllib.request.Request(url)
+    with urllib.request.urlopen(response) as respons:
+        
+        """read the content as bytes"""
+        content = respons.read()
+        
+        """Decode the content to utf8"""
+        content_utf8 = content.decode('utf-8')
+        
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content_utf8))
