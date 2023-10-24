de0e2a929049523871ea722a7c2c4400e0a64384
Fikayomi31 <fikayoogundijo@gmail.com>
2023-10-24 19:24:32 +0000

    write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
+
 const fs = require('fs');
 const fileName = process.argv[2];
 const fileContent = process.argv[3];
 
-fs.writeFile(fileName, fileContent, 'utf-8', (error, content) => {
+fs.writeFile(fileName, fileContent, 'utf-8', (error) => {
   if (error) {
     console.log(error);
-  } else {
-    console.log(content);
   }
 });
