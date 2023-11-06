29d04d58e25eb368f5239d889018b114b41afd10
Fikayomi31 <fikayoogundijo@gmail.com>
2023-11-06 15:44:23 -0800

    Star Wars movies

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,5 @@
+$.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+  data.results.forEach(function (film) {
+    $("<li>").text(film.title).appendTo("ul#list_movies");
+  });  
+});
