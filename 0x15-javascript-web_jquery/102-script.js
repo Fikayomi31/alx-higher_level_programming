d31d4d72c7625492734464c5a05e6a2ea0d9b5d6
Fikayomi31 <fikayoogundijo@gmail.com>
2023-11-06 17:37:40 -0800

    Say hello to everybody

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('INPUT#btn_translate').click(function () {
+        const language_code = $('INPUT#language_code').val();
+        $.getJSON(`https://www.fourtonfish.com/hellosalut/hello/?lang=${language_code}`, function (data) {
+            $('#hello').text(data.hello);    
+        });
+    });
+});
